7aeab5753899cbf3a01091906d4760ef14ce1a86
Nickname <88354270+Nickn4meDEV@users.noreply.github.com>
2023-04-06 19:05:31 +0200

    Zadanie 2

--- podstawy_programowania/06.04.2023/Zadanie2.cs
+++ podstawy_programowania/06.04.2023/Zadanie2.cs
@@ -1,18 +1,24 @@
-const uint n = 7;
-uint[] t = new uint[n];
-uint suma = 1;
-
-Console.WriteLine("Ciag Fibonacciego:\n");
-t[0] = 0;
-
-if (n > 1)
+class Zadanie2
 {
-    t[1] = 1;
-}
+    static void Main(string[] args)
+    {
+        const uint n = 7;
+        uint[] t = new uint[n];
+        uint suma = 1;
 
-for (int i = 2; i < n; i++)
-{
-    t[i] = t[i - 2] + t[i - 1];
-    suma += t[i];
+        Console.WriteLine("Ciag Fibonacciego:\n");
+        t[0] = 0;
+
+        if (n > 1)
+        {
+            t[1] = 1;
+        }
+
+        for (int i = 2; i < n; i++)
+        {
+            t[i] = t[i - 2] + t[i - 1];
+            suma += t[i];
+        }
+        Console.WriteLine("\nSuma N-pierwszych wynosi: " + suma);
+    }
 }
-Console.WriteLine("\nSuma N-pierwszych wynosi: " + suma);
